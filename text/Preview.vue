20a4019a5e4ed04714d105279761c737c484e2b8
hashrock <hashrock@users.noreply.github.com>
2020-10-12 02:12:38 +0900

    wip

---
+++ text/Preview.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <h1>Preview</h1>
+    <textarea v-model="contents" name id cols="50" rows="50"></textarea>
+  </div>
+</template>
+
+<script>
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+import { firebaseConfig } from "../share/config";
+import debounce from "lodash.debounce";
+import Anydown from "@anydown/anydown-core";
+import "@anydown/anydown-core/dist/anydown-core.es.css";
+import { compile, getYMD } from "./util";
+
+// Initialize Firebase
+
+export default {
+  data() {
+    return {
+      contents: ""
+    };
+  },
+  mounted() {
+    firebase.initializeApp(firebaseConfig);
+    const auth = firebase.auth();
+    let db = firebase.firestore();
+
+    var docRef = db.collection("users").doc(this.$router.uid);
+
+    docRef
+      .get()
+      .then(function(doc) {
+        if (doc.exists) {
+          console.log("Document data:", doc.data());
+        } else {
+          // doc.data() will be undefined in this case
+          console.log("No such document!");
+        }
+      })
+      .catch(function(error) {
+        console.log("Error getting document:", error);
+      });
+  }
+};
+</script>
+
+<style>
+</style>
--- text/index.js
+++ text/index.js
@@ -1,12 +1,29 @@
 import Vue from "vue";
 import App from "./App.vue";
 import VueRouter from 'vue-router'
+import Preview from './Preview.vue'
+import Editor from './Editor.vue'
 
-Vue.use(VueRouter)
+// Vue.use(VueRouter)
 Vue.config.productionTip = false;
+Vue.use(VueRouter)
+const router = new VueRouter({
+  routes: [
+    {
+      path: '/preview/:uid/:docid/:pass',
+      component: Preview
+    },
+    {
+      path: '/',
+      component: Editor
+    },
+  ]
+})
+
 
 /* eslint-disable no-new */
 new Vue({
   el: "#app",
-  render: h => h(App)
+  render: h => h(App),
+  router
 });
